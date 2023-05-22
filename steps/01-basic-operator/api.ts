f5d2efffe1ef2ce05160704e72c30a8f592b3f07
Lou-Theo <28760135+lou-theo@users.noreply.github.com>
2023-05-22 20:24:33 +0200

    feat: add tests for 01-basic-operator

--- steps/01-basic-operator/api.ts
+++ steps/01-basic-operator/api.ts
@@ -3,13 +3,24 @@ import { Order } from './models';
 
 let hasEmittedError = false;
 
+export const validOrders: Order[] = [
+  { isValid: true, numberOfItems: 1 },
+  { isValid: true, numberOfItems: 2 },
+  { isValid: true, numberOfItems: 3 },
+];
+
 export function getOrders(increaseDifficulty: boolean = false): Observable<Order[]> {
   return new Observable((subscriber) => {
-    subscriber.next([{ isValid: true, numberOfItems: 1 }, { isValid: false, numberOfItems: -10 }, { isValid: true, numberOfItems: 3 }]);
+    subscriber.next([
+      { isValid: true, numberOfItems: 1 },
+      { isValid: false, numberOfItems: -10 },
+      { isValid: true, numberOfItems: 3 },
+    ]);
     if (increaseDifficulty && !hasEmittedError) {
       hasEmittedError = true;
       subscriber.error('Network error, please try again later');
     }
-    subscriber.next([{ isValid: true, numberOfItems: 1 }, { isValid: true, numberOfItems: 2 }, { isValid: true, numberOfItems: 3 }]);
+    subscriber.next(validOrders);
+    subscriber.complete();
   });
 }
